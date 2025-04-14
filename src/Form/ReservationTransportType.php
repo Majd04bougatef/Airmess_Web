d9768803886ff7049464046e6e20327214214f49
Majd04bougatef <127989172+Majd04bougatef@users.noreply.github.com>
2025-04-14 21:41:46 +0100

    controle de saisie added in the  file formType of reservation and station

--- src/Form/ReservationTransportType.php
+++ src/Form/ReservationTransportType.php
@@ -9,6 +9,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ReservationTransportType extends AbstractType
 {
@@ -18,18 +19,42 @@ class ReservationTransportType extends AbstractType
             ->add('dateRes', null, [
                 'widget' => 'single_text',
                 'attr' => ['class' => 'form-control'],
-                'label_attr' => ['class' => 'label']
+                'label_attr' => ['class' => 'label'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'La date de réservation est obligatoire']),
+                    new Assert\GreaterThanOrEqual([
+                        'value' => 'today',
+                        'message' => 'La date de réservation doit être aujourd\'hui ou dans le futur'
+                    ])
+                ]
             ])
             ->add('dateFin', null, [
                 'widget' => 'single_text',
                 'attr' => ['class' => 'form-control'],
-                'label_attr' => ['class' => 'label']
+                'label_attr' => ['class' => 'label'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'La date de fin est obligatoire']),
+                    new Assert\GreaterThan([
+                        'propertyPath' => 'parent.all[dateRes].data',
+                        'message' => 'La date de fin doit être après la date de réservation'
+                    ])
+                ]
             ])      
-            ->add('nombreVelo',null,['attr' => ['class' => 'form-control'],
-                'label_attr' => ['class' => 'label']])
-
-               
-        ;
+            ->add('nombreVelo', null, [
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'label'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Le nombre de vélos est obligatoire']),
+                    new Assert\GreaterThan([
+                        'value' => 0,
+                        'message' => 'Le nombre de vélos doit être supérieur à 0'
+                    ]),
+                    new Assert\LessThanOrEqual([
+                        'value' => 10,
+                        'message' => 'Le nombre de vélos ne peut pas dépasser 10'
+                    ])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/StationType.php
+++ src/Form/StationType.php
@@ -10,49 +10,103 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Intl\Countries;
-
+use Symfony\Component\Validator\Constraints as Assert;
 
 class StationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-    ->add('nom', null, [
-        'attr' => ['class' => 'input-text'],
-    ])
-    ->add('latitude', null, [
-        'attr' => ['class' => 'input-text'],
-    ])
-    ->add('longitude', null, [
-        'attr' => ['class' => 'input-text'],
-    ])
-    ->add('capacite', null, [
-        'attr' => ['class' => 'input-text'],
-    ])
-    ->add('nombreVelo', null, [
-        'attr' => ['class' => 'input-text'],
-    ])
-    ->add('typeVelo', ChoiceType::class, [
-        'choices' => [
-            'Vélo électrique' => 'velo électrique',
-            'Vélo urbain' => 'velo urbain',
-            'Vélo de route' => 'velo de route',
-        ],
-        'attr' => ['class' => 'input-text'],
-        'expanded' => false, // Afficher sous forme de boutons radio
-        'multiple' => false, // Permettre un seul choix
-    ])
-    ->add('prixHeure', null, [
-        'attr' => ['class' => 'input-text'],
-    ])
-    ->add('pays', ChoiceType::class, [
-        'choices' => $this->getCountryList(),
-        'attr' => ['class' => 'input-text'],
-        'expanded' => false,
-        'multiple' => false,
-    ]);
-
-   ;
+            ->add('nom', null, [
+                'attr' => ['class' => 'input-text'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Le nom de la station est obligatoire']),
+                    new Assert\Length([
+                        'max' => 255,
+                        'maxMessage' => 'Le nom de la station ne peut pas dépasser {{ limit }} caractères'
+                    ])
+                ]
+            ])
+            ->add('latitude', null, [
+                'attr' => ['class' => 'input-text'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'La latitude est obligatoire']),
+                    new Assert\Type([
+                        'type' => 'float',
+                        'message' => 'La latitude doit être un nombre'
+                    ])
+                ]
+            ])
+            ->add('longitude', null, [
+                'attr' => ['class' => 'input-text'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'La longitude est obligatoire']),
+                    new Assert\Type([
+                        'type' => 'float',
+                        'message' => 'La longitude doit être un nombre'
+                    ])
+                ]
+            ])
+            ->add('capacite', null, [
+                'attr' => ['class' => 'input-text'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'La capacité est obligatoire']),
+                    new Assert\GreaterThan([
+                        'value' => 10,
+                        'message' => 'La capacité doit être supérieure à 10'
+                    ])
+                ]
+            ])
+            ->add('nombreVelo', null, [
+                'attr' => ['class' => 'input-text'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Le nombre de vélos est obligatoire']),
+                    new Assert\LessThanOrEqual([
+                        'propertyPath' => 'parent.all[capacite].data',
+                        'message' => 'Le nombre de vélos ne peut pas être supérieur à la capacité'
+                    ])
+                ]
+            ])
+            ->add('typeVelo', ChoiceType::class, [
+                'choices' => [
+                    'Vélo électrique' => 'velo électrique',
+                    'Vélo urbain' => 'velo urbain',
+                    'Vélo de route' => 'velo de route',
+                ],
+                'attr' => ['class' => 'input-text'],
+                'expanded' => false,
+                'multiple' => false,
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Le type de vélo est obligatoire']),
+                    new Assert\Choice([
+                        'choices' => ['velo électrique', 'velo urbain', 'velo de route'],
+                        'message' => 'Veuillez choisir un type de vélo valide'
+                    ])
+                ]
+            ])
+            ->add('prixHeure', null, [
+                'attr' => ['class' => 'input-text'],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Le prix par heure est obligatoire']),
+                    new Assert\GreaterThan([
+                        'value' => 0,
+                        'message' => 'Le prix par heure doit être supérieur à 0'
+                    ])
+                ]
+            ])
+            ->add('pays', ChoiceType::class, [
+                'choices' => $this->getCountryList(),
+                'attr' => ['class' => 'input-text'],
+                'expanded' => false,
+                'multiple' => false,
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Le pays est obligatoire']),
+                    new Assert\Choice([
+                        'callback' => [$this, 'getCountryList'],
+                        'message' => 'Veuillez choisir un pays valide'
+                    ])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
