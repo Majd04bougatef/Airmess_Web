f58dcae71dd36e042e37e3695fdc233427ac7b41
oussemabb20 <benelhaj.oussema@esprit.tn>
2025-04-15 18:57:48 +0100

    fix login with google

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -20,7 +20,7 @@ class User
     private string $name;
 
     #[ORM\Column(type: "string", length: 255)]
-    private string $prenom;
+    private ?string $prenom = null;
 
     #[ORM\Column(type: "string", length: 255, unique: true)]
     private string $email;
@@ -32,7 +32,7 @@ class User
     private string $roleUser;
 
     #[ORM\Column(name: "dateNaiss", type: "date")]
-    private \DateTimeInterface $dateNaiss;
+    private ?\DateTimeInterface $dateNaiss = null;
 
     #[ORM\Column(name: "phoneNumber", type: "string", length: 20)]
     private string $phoneNumber;
@@ -95,12 +95,12 @@ class User
         return $this;
     }
 
-    public function getPrenom(): string
+    public function getPrenom(): ?string
     {
         return $this->prenom;
     }
 
-    public function setPrenom(string $prenom): self
+    public function setPrenom(?string $prenom): self
     {
         $this->prenom = $prenom;
         return $this;
@@ -139,12 +139,12 @@ class User
         return $this;
     }
 
-    public function getDateNaiss(): \DateTimeInterface
+    public function getDateNaiss(): ?\DateTimeInterface
     {
         return $this->dateNaiss;
     }
 
-    public function setDateNaiss(\DateTimeInterface $dateNaiss): self
+    public function setDateNaiss(?\DateTimeInterface $dateNaiss): self
     {
         $this->dateNaiss = $dateNaiss;
         return $this;
